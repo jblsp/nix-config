65474eeabd1188753aa36a04e4af87c62cbc85f1
Joe <48526917+jblsp@users.noreply.github.com>
2024-11-07 11:18:17 -0500

    Update bootstrap shell

--- shell.nix
+++ shell.nix
@@ -1,10 +1,9 @@
-{pkgs ? import <nixpkgs> {}}: {
-  default = pkgs.mkShell {
-    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [
-      nix
-      home-manager
-      git
-    ];
-  };
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+  nativeBuildInputs = with pkgs; [
+    nix
+    home-manager
+    git
+  ];
 }
